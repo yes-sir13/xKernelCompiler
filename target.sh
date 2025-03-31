55b8718550e77677c9c82d89a01de1ef61d98b4e
yes-sir13 . <gmkgamers13@gmail.com>
2025-03-31 09:26:55 +0000

    update

--- target.sh
+++ target.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR= # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG= # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR= # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER= # Change with your own name or else.
-export KBUILD_BUILD_HOST= # Change with your own hostname.
+KERNEL_ROOTDIR=/workspace/build/kernel/common # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=gki_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=/workspace/build/zyc-21 # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=Sir # Change with your own name or else.
+export KBUILD_BUILD_HOST=YesSir # Change with your own hostname.
